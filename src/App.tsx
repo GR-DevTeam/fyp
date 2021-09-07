667c58f3c9eab0f617f0d0dc3926fb7665df1fd4
MaxHMoyano <maxhmoyano@gmail.com>
2021-09-07 11:48:53 -0300

    added styled components & storybook into repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      {/* Aca van para testear los componentes hasta que Dani termine el #9 */}
+    </div>
+  );
 }
 
 export default App;
