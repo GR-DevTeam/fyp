772d05404df48cce448edf630d1aba313f895637
Franco Martinetto <francomartinetto@hotmail.com>
2021-09-08 10:48:59 -0300

    Added Import Sidebar Button to App for Testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import InputText from './components/shared/InputText/InputText';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/theme';
 import GlobalStyle from './theme/GlobalTheme';
-
+import SidebarButton from './components/SidebarButton/SidebarButton';
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className='App'>
-        <InputText icon={faSearch} />
+      <div className="App">
         {/* Aca van para testear los componentes hasta que Dani termine el #9 */}
+        <InputText icon={faSearch} />
+        <SidebarButton icon={faSearch} text="search" />
       </div>
     </ThemeProvider>
   );
