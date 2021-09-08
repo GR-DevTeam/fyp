570da83c7ab9d929ef6081ed2d017312a698922c
juanCruzSalina <salinajuancruz@gmail.com>
2021-09-08 14:58:40 -0300

    sidebar creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import Sidebar from './components/Sidebar/Sidebar';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
   return (
-    <div className='App'>
+    <Wrapper>
+      <Sidebar />
       {/* Aca van para testear los componentes hasta que Dani termine el #9 */}
-    </div>
+    </Wrapper>
   );
 }
 
