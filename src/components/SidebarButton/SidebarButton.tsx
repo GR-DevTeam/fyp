71f629fc19ff5087bcca29b4deef3447b7b1da7f
Franco Martinetto <66968994+FrancoMartinetto@users.noreply.github.com>
2021-09-10 18:09:38 -0300

    Feature/fyp 10/sidebar button creation (#12)
    * fixed styling on InputText
    
    * added Global theme & standard color palette
    
    * New: Created Basic Sidebar Button structure
    
    * Added Import Sidebar Button to App for Testing
    
    * Fixed Button component Layout
    
    * Fix Added some padding to center text,also change example of component
    
    * Chore change color to white
    
    * Chore cleaned the App component
    
    * Chore cleaned unused styled component
    
    Co-authored-by: MaxHMoyano <maxhmoyano@gmail.com>
    Co-authored-by: MaxHMoyano <45719839+MaxHMoyano@users.noreply.github.com>

---
+++ src/components/SidebarButton/SidebarButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+export interface ISidebarButton {
+  text?: string;
+  icon: IconDefinition;
+}
+
+const StyledSidebarButton = styled.button`
+  color: white;
+  cursor: pointer;
+  border: none;
+  background: none;
+  display: flex;
+  justify-content: top;
+  align-items: center;
+  font-size: 2rem;
+  padding: 1rem 1rem;
+  :hover {
+    transition: all 0.3s ease;
+    color: ${(props) => props.theme.colors.primary};
+    transform: translateX(10px);
+  }
+`;
+const Text = styled.div`
+  padding-left: 0.4rem;
+  padding-bottom: 0.5rem;
+`;
+
+const SidebarButton: React.FC<ISidebarButton> = (props) => {
+  return (
+    <StyledSidebarButton>
+      <FontAwesomeIcon icon={props.icon} />
+      <Text>{props.text}</Text>
+    </StyledSidebarButton>
+  );
+};
+
+export default SidebarButton;
--- src/components/shared/InputText/InputText.tsx
+++ src/components/shared/InputText/InputText.tsx
@@ -1,23 +1,58 @@
+import { faSearch, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
 
 export interface IInputText {
-  placeholder: string;
-  icon: React.ReactNode;
+  placeholder?: string;
+  icon: IconDefinition;
 }
+
+const defaultProps: IInputText = {
+  placeholder: 'Search...',
+  icon: faSearch,
+};
+
+const Icon = styled(FontAwesomeIcon)`
+  color: #3a3a3a;
+  background: #f0f0f0;
+  flex: 0 0;
+  padding: 1rem;
+  border-radius: 0 5px 5px 0;
+`;
 
 const StyledInput = styled.input`
   font-size: 1rem;
-  background: #000;
-  color: #fff;
-  padding: 1rem;
-  border-radius: 5px;
+  color: black;
+  background: transparent;
   outline: none;
   border: none;
+  padding: 0 1rem;
+  font-weight: bold;
+  &::-webkit-input-placeholder {
+    opacity: 1;
+  }
 `;
 
-const InputText = (props: IInputText) => {
-  return <StyledInput placeholder={props.placeholder} />;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background: transparent;
+  width: fit-content;
+  box-shadow: 0.5rem 0.25rem 0.5rem #0002;
+  border-radius: 5px;
+  border: 1px solid #0001;
+`;
+
+const InputText: React.FC<IInputText> = (props) => {
+  return (
+    <Wrapper>
+      <StyledInput placeholder={props.placeholder} />
+      <Icon icon={props.icon} />
+    </Wrapper>
+  );
 };
+
+InputText.defaultProps = defaultProps;
 
 export default InputText;
