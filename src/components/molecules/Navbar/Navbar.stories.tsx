e28e80b944578e858e6c5c82cc8e2ccbfb848e98
DiegoGastonGarcia <82785058+DiegoGastonGarcia@users.noreply.github.com>
2021-09-28 12:50:20 -0300

    Feature/fyp 8/navbar creation (#16)
    * cleaning and updating with styled
    
    * first try without final components
    
    * first try
    
    * imput & torybook
    
    * app cleaning
    
    * theme aplication
    
    * grid-areas

---
+++ src/components/molecules/Navbar/Navbar.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Navbar from './Navbar';
+
+
+export default {
+    title: 'Components/Molecules/Navbar',
+    component: Navbar,
+  } as ComponentMeta<typeof Navbar>;
+
+  const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
+
+
+  export const Default = Template.bind({});
+
