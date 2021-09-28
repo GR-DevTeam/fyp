8b6bf8792abbf32c279d5f0c4c556d7faf7eec36
DiegoGastonGarcia <82785058+DiegoGastonGarcia@users.noreply.github.com>
2021-09-28 15:39:47 -0300

    navbar fixed (#21)

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -6,14 +6,14 @@ import Input from '../../atoms/Input/Input';
 
 
 const StyledNavbar = styled.div`
-    width: 100vw;
+    width: 100%;
     height: ${props => props.theme.measures.l};
     grid-area: navbar;
     background: ${props => props.theme.colors.dark};
     color: ${props => props.theme.colors.light};
     display: flex;
     justify-content: space-between;
-    padding: ${props => props.theme.spacing.xs};
+    padding: ${props => props.theme.spacing.xxs};
 `;
 
 const StyledLogo = styled.div`
