748739061b4ed55aa4de5b0b11b2fd866a53476a
juanCruzSalina <salinajuancruz@gmail.com>
2021-09-28 08:14:05 -0300

    Added children control in storybook

--- src/components/atoms/Text/Text.stories.tsx
+++ src/components/atoms/Text/Text.stories.tsx
@@ -5,10 +5,17 @@ import Text from './Text';
 export default {
   title: 'Components/Atoms/Text',
   component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum volutpat arcu eu dapibus. Donec cursus porttitor cursus. In tincidunt.',
+  },
+  argTypes: {
+    children: { control: 'text' },
+  },
 } as ComponentMeta<typeof Text>;
 
 const Template: ComponentStory<typeof Text> = (args) => (
-  <Text {...args}> Example </Text>
+  <Text {...args}>{args.children}</Text>
 );
 
 export const Default = Template.bind({});
