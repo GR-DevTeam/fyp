44955e21a159127861abd2001b5be92897a2e84d
Diego Garcia <d.g.garcia@outlook.com>
2021-09-30 20:51:28 -0300

    basics

--- src/components/molecules/Hero/Hero.tsx
+++ src/components/molecules/Hero/Hero.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import styled from 'styled-components';
+import Text from '../../atoms/Text/Text';
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-row: 2;
+    align-items: center;
+`;
+
+const Loquereciba = styled.div`
+    background: ${props => props.theme.colors.primary};
+    color: ${props => props.theme.colors.light};
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-row: 2;
+    justify-content: center;
+    align-items: center;
+`;
 
 const Hero: React.FC = () => {
     return (
-        <div>
-            
-        </div>
+            <Loquereciba>
+                <Text size={'l'}> hola juancarlo</Text>
+                ola
+            </Loquereciba>
     )
 }
 
