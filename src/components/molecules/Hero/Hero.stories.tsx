300295180d6a390d90434fee8c3ed72c75daf058
Diego Garcia <d.g.garcia@outlook.com>
2021-09-30 04:02:24 -0300

    storybook

---
+++ src/components/molecules/Hero/Hero.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Hero from './Hero';
+
+
+export default {
+    title: 'Components/Molecules/Hero',
+    component: Hero,
+  } as ComponentMeta<typeof Hero>;
+
+  const Template: ComponentStory<typeof Hero> = (args) => <Hero {...args} />;
+
+
+  export const Default = Template.bind({});
