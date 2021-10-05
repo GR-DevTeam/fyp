35889e2328c94cecfcbc53de61fd77d58d81b666
Victor Florentin <victorsantiagoflorentin@gmail.com>
2021-10-04 21:19:51 -0300

    [FYP-24] improvements in Loading component, added Spinner Icon

--- src/components/atoms/Loading/Loading.tsx
+++ src/components/atoms/Loading/Loading.tsx
@@ -4,34 +4,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompass, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 export interface ILoading {
-  placeholder?: string;
   icon?: IconDefinition;
 }
 
 const Spinner = styled(FontAwesomeIcon)`
-  color: ${(props) => props.theme.colors.dark};
-  background: ${(props) => props.theme.colors.light};
-  flex: 0 0;
-  padding: 1rem;
+  color: ${(props) => props.theme.colors.primary};
+  background: #0000;
+  font-size: ${(props) => props.theme.measures.xxl};
+  height: 10%;
+  width: 10%;
+  //flex: 0 0;
+`;
+
+const Background = styled.div`
+  height: 100%;
+  width: 100%;
+  background: #cac1c15e;
+  backdrop-filter: blur(5px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const StyledLoading = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: #404240;
   color: ${(props) => props.theme.colors.light};
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
   padding: ${(props) => props.theme.spacing.xxs};
-  img {
-    align-self: center;
-  }
 `;
 
 const Loading: React.FC<ILoading> = (props) => {
   return (
     <StyledLoading>
-      <Spinner icon={props.icon as IconDefinition} />
+      <Background>
+        <Spinner icon={props.icon as IconDefinition} spin />
+      </Background>
     </StyledLoading>
   );
 };
