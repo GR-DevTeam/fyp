f8ab433c1a2cc8d35127f3305ec181025df5b6b2
MaxHMoyano <45719839+MaxHMoyano@users.noreply.github.com>
2021-10-19 11:18:56 -0300

    feat: added slice for menu state globally (#28)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-
+import menuReducer from '../features/menu/menuSlice';
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    menu: menuReducer,
   },
 });
 
--- src/components/molecules/Sidebar/Sidebar.stories.tsx
+++ src/components/molecules/Sidebar/Sidebar.stories.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Sidebar from './Sidebar';
-import { faUserFriends } from '@fortawesome/free-solid-svg-icons';
-import theme from '../../../theme/theme';
 
 export default {
   title: 'Components/Molecules/Sidebar',
@@ -11,13 +9,4 @@ export default {
 
 const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
 export const Default = Template.bind({});
-Default.args = {
-  items: [
-    {
-      icon: faUserFriends,
-      text: 'example',
-      color: theme.colors.light,
-      hoverColor: theme.colors.primary,
-    },
-  ],
-};
+Default.args = {};
