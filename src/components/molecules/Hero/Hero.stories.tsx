08115332379a879deaaeb105cc21879b1bfca4d3
Diego Garcia <d.g.garcia@outlook.com>
2021-10-28 14:49:36 -0300

    lower cases, button click and styling

--- src/components/molecules/Hero/Hero.stories.tsx
+++ src/components/molecules/Hero/Hero.stories.tsx
@@ -9,9 +9,20 @@ export default {
 
 const Template: ComponentStory<typeof Hero> = (args) => <Hero {...args} />;
 
+
 export const Default = Template.bind({});
 Default.args = {
   title: 'Hero title',
-  description: 'Hero description',
+  buttonText: 'Hero button',
   onButtonClick: (ev) => console.log(ev),
+}
+
+export const CompleteHero = Template.bind({});
+CompleteHero.args = {
+  title: 'Hero title',
+  description: 'Hero description',
+  buttonText: 'Hero button',
+  onButtonClick: (ev) => console.log(ev),
+  terms: 'Hero terms',
 };
+
--- src/components/molecules/Hero/Hero.tsx
+++ src/components/molecules/Hero/Hero.tsx
@@ -1,42 +1,43 @@
-import React from 'react';
+import React from 'react'
 import styled from 'styled-components';
 import Text from '../../atoms/Text/Text';
 import Button from '../../atoms/Button/Button';
 import Heading from '../../atoms/Heading/Heading';
 
 export interface IHero {
-  // TODO: Add backgroundColor & optional button rendering
-  // Conditional rendering info: https://reactjs.org/docs/conditional-rendering.html
-  title: string;
-  description: string;
-  button: string;
-  terms: string;
-  onButtonClick: (ev: any) => any;
+    title: string;
+    description?: string;
+    buttonText?: string;
+    terms?: string;
+    onButtonClick?: (ev: any) => any;
 }
 
-const Wrapper = styled.div`
-  background: ${(props) => props.theme.colors.primary};
-  color: ${(props) => props.theme.colors.light};
-  width: 100%;
-  height: 100%;
-  display: grid;
-  grid-row: 4;
-  justify-content: center;
-  align-items: center;
-  justify-items: center;
+
+
+const Wrapper = styled.div<IHero>`
+    width: 100%;
+    height: 100%;
+    background-color: ${(props) => props.theme.colors.primary};
+    color: ${(props) => props.theme.colors.dark};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+    grid-template-columns: 1fr 4fr;
 `;
 
 const Hero: React.FC<IHero> = (props) => {
-  return (
-    <Wrapper>
-      <Heading size={'l'}>{props.title}</Heading>
-      <Text size={'m'}>{props.description}</Text>
-      <Button onClick={props.onButtonClick} isInverted>
-        {props.button}
-      </Button>
-      <Text size={'s'}>{props.terms}</Text>
-    </Wrapper>
-  );
-};
+    return (
+        <Wrapper {...props}>
+            <Heading size={'l'}>{props.title}</Heading>
+            <Text size={'l'}>{props.description}</Text>
+            <Button isInverted>{props.buttonText}</Button>
+            <Text size={'s'}>{props.terms}</Text>
+            </Wrapper>
+    )
+}
 
-export default Hero;
+
+export default Hero
+
