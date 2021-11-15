6411a6d2651c9fdcbd5f8d647777ea8b9694214c
juanCruzSalina <salinajuancruz@gmail.com>
2021-11-15 19:20:39 -0300

    feat: styling and functionality added

--- src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/molecules/Breadcrumbs/Breadcrumbs.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import styled from 'styled-components';
 
-const Breadcrumbs = () => {
+import { BreadcrumbItem, IBreadcrumbItem } from './BreadcrumbItem';
+
+export interface IBreadcrumb {
+  items: IBreadcrumbItem[]
+}
+
+const BreadContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  box-shadow: -1px 0px 0px 0px ${props => props.theme.colors['grey-400']};
+`;
+
+const Breadcrumbs: React.FC<IBreadcrumb> = (props) => {
   return (
-    <div>
-      
-    </div>
+    <BreadContainer>
+      {props.items.map((item, idx )=> {
+        return <BreadcrumbItem {...item} key={idx} />
+      })}
+    </BreadContainer>
   )
 }
 
